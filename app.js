108680548f729c5205f8c5071c4fce17e024b12d
sismitrii <floguerin156@gmail.com>
2022-06-29 16:45:56 +0200

    ➕ ✨ [app.js] : mongoDb connected with mongoose

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://UserOfOC:Azerty01.@clusteroc.ndiye.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true, // don't find why this is needed
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
 app.use((req, res,next)=> {
-    res.end("test");
+    res.status(200).end("other test");
 })
 
 module.exports = app;

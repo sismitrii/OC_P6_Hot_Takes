e9ea491f0526c7e0934592e5d35c6094326abf3e
sismitrii <floguerin156@gmail.com>
2022-06-29 15:56:07 +0200

    ✨ [server] : server initialisation done

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
+/*=============================================================*/
+/*------------------------ IMPORT -----------------------------*/
+/*=============================================================*/
+
 const http = require('http'); // how include https ?
 const app = require('./app');
+
+/*=============================================================*/
+/*---------------------- FUNCTIONS ----------------------------*/
+/*=============================================================*/
 
 /* === Return a valid port === */
 function normalizePort(port){
@@ -15,9 +23,42 @@ function normalizePort(port){
     return false;
 }
 
-const port = normalizePort(process.env.PORT || '3000')
+/* === Explaination of error ===*/
+function errorHandler(error){
+    if (error.syscall !== 'listen'){
+        throw error;
+    }
+
+    switch (error.code) {
+        case 'EACCES':
+          console.error(bind + ' requires elevated privileges.');
+          process.exit(1); // end the process which is running
+          break;
+        case 'EADDRINUSE':
+          console.error(bind + ' is already in use.');
+          process.exit(1);
+          break;
+        default:
+          throw error;
+      }
+}
+
+/*=============================================================*/
+/*-------------------------- MAIN -----------------------------*/
+/*=============================================================*/
+
+const port = normalizePort(process.env.PORT || '3000') // process.env.PORT for hosting // 3000 for developpement
 app.set('port', port)// to give to express app on which port is it going to run
 // J'ai testé ça fonctionne sans donc pourquoi le mettre..
+
 const server = http.createServer(app);
+
+const address = server.address();  // adress is the IP adress of the server if it's hosted and null if it's not
+const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+
+server.on('error', errorHandler);
+server.on('listening', () => {
+    console.log('Listening on ' + bind);
+})
 
 server.listen(port); // process.env.PORT = port par defaut proposé par la plateforme de developpement

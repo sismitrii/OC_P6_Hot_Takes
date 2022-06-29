e57d475be034d0d1eac8af1d7dde1c89a8f4bc43
sismitrii <floguerin156@gmail.com>
2022-06-29 14:55:25 +0200

    🚧 [server] : normalizePort use to listen a valid port

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const http = require('http'); // how include https ?
 const app = require('./app');
 
+/* === Return a valid port === */
+function normalizePort(port){
+    const numberPort = parseInt(port, 10);
+
+    /*if (isNaN(port)) {
+      return val;
+    }*/
+    if (numberPort >= 0) { //if a negative port is possible 
+      return numberPort;
+    }
+    console.log("Wrong PORT value");
+    return false;
+}
+
+const port = normalizePort(process.env.PORT || '3000')
+app.set('port', port)// to give to express app on which port is it going to run
+// J'ai testé ça fonctionne sans donc pourquoi le mettre..
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000); // process.env.PORT = port par defaut proposé par la plateforme de developpement
+server.listen(port); // process.env.PORT = port par defaut proposé par la plateforme de developpement
